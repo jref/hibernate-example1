5e6f819cc5345a001aed457f91648f67343fdc07
Oleksandr Navka <Oleksandr.Navka@harmonicinc.com>
2017-04-03 16:02:09 +0300

    Initial commit

---
+++ src/main/java/ua/com/codespace/hibaranate/Book.java
@@ -0,0 +1,69 @@
+package ua.com.codespace.hibaranate;
+
+public class Book {
+
+    private int id;
+    private String bookName;
+    private String authorName;
+
+    public Book() {
+    }
+
+    public Book(String bookName, String authorName) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", bookName='" + bookName + '\'' +
+                ", authorName='" + authorName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        if (id != book.id) return false;
+        if (bookName != null ? !bookName.equals(book.bookName) : book.bookName != null) return false;
+        return authorName != null ? authorName.equals(book.authorName) : book.authorName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (bookName != null ? bookName.hashCode() : 0);
+        result = 31 * result + (authorName != null ? authorName.hashCode() : 0);
+        return result;
+    }
+}
